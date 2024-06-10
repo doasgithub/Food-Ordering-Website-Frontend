8c405d811751e8e0be3b6cf4f7a1d44e6521a701
doasgithub <doas5978@gmail.com>
2024-06-10 16:58:37 +0530

    added search bar,cuisine filter,sort filter,pagination

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,28 @@
 import landingImage from "../assets/landing.png"
 import appDownloadImage from "../assets/appDownload.png"
+import { useNavigate } from "react-router-dom";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
     return(
         <div className="flex flex-col gap-12">
             <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
             <h1 className="text-5xl font-bold tracking-tight text-orange-700">
-                   AAJ KA MENU
+            SATISFY YOUR CRAVINGS
             </h1>
             <span className="text-xl">Food is just a click away!</span>
+            <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
 
             </div>
             <div className="grid md:grid-cols-2 gap-5">
@@ -18,7 +32,7 @@ const HomePage = () => {
                           Order takeaway even faster!
                         </span>
                    <span>
-                       Download the KhanaMangao App for faster ordering and personalised
+                       Download the Crave Crafters App for faster ordering and personalised
                         recommendations
                      </span>
                       <img src={appDownloadImage} />
